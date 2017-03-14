d7fb57d490a4e4ac0d7eb1af08536ee325d60820
natapunch <natapunch7@gmail.com>
2017-03-14 11:02:35 +0000

    New branch dev

--- public/index.php
+++ public/index.php
@@ -3,8 +3,10 @@
  *
  */
 $loader = require '../vendor/autoload.php';
-$loader->addPsr4("My\\", dirname(__FILE__).'/../src/');
+$loader->addPsr4("My\\", dirname(__FILE__) . '/../src/');
+
 $page = new My\Page();
 $page->show();
 echo "<pre>";
 print_r($_SERVER);
+
